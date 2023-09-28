48d44d305c5452302d471538c44eac738b0998d9
etgarbeg <etgarbeg@yahoo.com>
2023-09-28 14:25:48 +0300

    28/09

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Tabs from './assets/components/Tabs';
 import { UserProvider } from './assets/UserContext/UserProvider';
+import { response } from 'express';
 
 
 const App = ({ navigation }) => {
+
+  const [backEndData, setBackEndData] = useState([{}]);
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data =>
+        setBackEndData(data)
+    )
+  })
+
+
+
 
   return (
     <UserProvider>
--- server/server.js
+++ server/server.js
@@ -1,29 +1,20 @@
-//using express
+
 const express = require('express');
-//using mongoDB
+
 const mongoose = require('mongoose');
-//create express object.
+
 const app = express();
 
-// Set up MongoDB connection - change adress
-try {
-    await mongoose.connect('mongodb://localhost/mydatabase', { useNewUrlParser: true });
-    console.log('MongoDB connected');
-} catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-}
 
-// Define routes
-app.get('/', (req, res) => {
-    try {
-        res.send('app');
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-    }
+app.get('/api', (req, res) => {
+
+    res.json({ "users": ["userOne", "userTwo"] })
+
 });
-// Start server
-app.listen(3000, () => console.log('Server started on port 3000'));
+
+
+
+app.listen(5000, () => console.log('Server started on port 5000'));
 
 
 
