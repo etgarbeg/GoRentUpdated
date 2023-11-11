8fda2e622cfd6911b5f64e7be185cf95f20325bd
etgarbeg <etgarbeg@yahoo.com>
2023-11-11 16:49:45 +0200

    11/11

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,10 +3,10 @@ class UserModel {
 
     //functions 
 
-    static async Register(username, password, image)//all props of register
+    static async Register(id, username, password, firstName, lastName, email, adress, creditCard, products, requested, image)//all props of register
     {
 
-        let user = { username, password, image }
+        let user = { id, username, password, firstName, lastName, email, adress, creditCard, products, requested, image }
         //check U/u
         return await new DB().Insert('users', user);
     }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,9 +4,11 @@ const UploadImage = require('../utils/upload');
 
 userRouter.post('/register', UploadImage, async (req, res) => {
     try {
-        let { username, password } = req.body;
+        //create user
+        let { username, password, firstName, lastName, email, adress, creditCard, products, requested } = req.body;
+        //save image
         let { cloudinaryRes } = req;
-        let user = await UserModel.Register(username, password, cloudinaryRes.secure_url);
+        let user = await UserModel.Register(username, password, firstName, lastName, email, adress, creditCard, products, requested, cloudinaryRes.secure_url);
         res.status(201).json({ user });
 
     } catch (error) {
