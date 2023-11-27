c5f21bea33b98dc90d3a58472f243ad015d38faf
etgarbeg <etgarbeg@yahoo.com>
2023-11-27 20:41:35 +0200

    Update UserContext.jsx

--- application/assets/UserContext/UserContext.jsx
+++ application/assets/UserContext/UserContext.jsx
@@ -109,7 +109,7 @@ export default function UserContextProvider({ children }) {
     const LoginUser = async () => {
         try {
             console.log("in Login user");
-            const response = await axios.post(`/login`, {
+            const response = await axios.post(`${API_BASE_URL}/register`, {
                 email,
                 password
             }, {
