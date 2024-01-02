9257d8402d2c11c2f2811fb55741f3ec0af8d2a9
etgarbeg <etgarbeg@yahoo.com>
2024-01-02 18:25:43 +0200

    /

--- application/screens/InboxScreen.jsx
+++ application/screens/InboxScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList } from 'react-native';
 
 import styles from '../assets/Styles/style';
 import SearchBar from '../assets/components/SearchBar';
@@ -9,7 +9,6 @@ import { UserContext } from '../../application/assets/UserContext/UserContext';
 const InboxScreen = ({ navigation }) => {
     const { currentUser, users } = useContext(UserContext);
     const currentTime = new Date();
-    const formattedTimestamp = currentTime.toISOString(); // ISO format: "2022-01-01T12:30:00.000Z"
 
     // Create a copy of messages and sort them by senderID
     const sortedMessages = [...currentUser.messages].sort((a, b) => a.senderID.localeCompare(b.senderID));
@@ -30,7 +29,6 @@ const InboxScreen = ({ navigation }) => {
                     source={require('../assets/images/icon/navbar/profile.png')}
                     style={{ width: '100%', height: '100%', resizeMode: 'cover' }}
                 />
-
             </View>
             <Text style={styles.titlee}>{currentUser.username}</Text>
             <SearchBar />
@@ -44,44 +42,43 @@ const InboxScreen = ({ navigation }) => {
                 </TouchableOpacity>
             </View>
 
-            <ScrollView>
-                <View style={styles.containerCollectionRow}>
-                    {sortedMessages.map((message, index) => (
-                        <TouchableOpacity
-                            key={message.senderID}
-                            style={styles.useMessageContainerInbox}
-                            onPress={() => navigation.navigate('SingleChat', { senderID: message.senderID, productRequestedID: message.productRequestedID })}
-                        >{alert(message.senderID)}
-                            <Image
-                                style={styles.profilePictureContainerInbox}
-                                source={{ uri: findUserById(message.senderID)?.image }}
-                            />
+            <FlatList
+                data={sortedMessages}
+                keyExtractor={(item) => item.senderID}
+                renderItem={({ item }) => (
+                    <TouchableOpacity
+                        style={styles.useMessageContainerInbox}
+                        onPress={() => navigation.navigate('SingleChat', { senderID: item.senderID, productRequestedID: item.productRequestedID })}
+                    >
+                        <Image
+                            style={styles.profilePictureContainerInbox}
+                            source={{ uri: findUserById(item.senderID)?.image }}
+                        />
 
-                            <View style={styles.itemBoxInbox}>
-                                <Text style={styles.usernameTitleInbox}>
-                                    {findUserById(message.senderID)?.username}
-                                </Text>
-                                <Text style={styles.timestampText}>
-                                    {new Date(message.timeStemp).toLocaleString('en-US', {
-                                        hour: 'numeric',
-                                        minute: 'numeric',
-                                        month: 'numeric',
-                                        day: 'numeric',
-                                    })}
-                                </Text>
-                                <Text style={styles.messageTextInbox}>
-                                    {message.txt} {' '}
-                                    {message.productRequestedID && (
-                                        <Text style={styles.productText}>
-                                            - {findProductById(message.productRequestedID)?.productName}
-                                        </Text>
-                                    )}
-                                </Text>
-                            </View>
-                        </TouchableOpacity>
-                    ))}
-                </View>
-            </ScrollView>
+                        <View style={styles.itemBoxInbox}>
+                            <Text style={styles.usernameTitleInbox}>
+                                {findUserById(item.senderID)?.username}
+                            </Text>
+                            <Text style={styles.timestampText}>
+                                {new Date(item.timeStemp).toLocaleString('en-US', {
+                                    hour: 'numeric',
+                                    minute: 'numeric',
+                                    month: 'numeric',
+                                    day: 'numeric',
+                                })}
+                            </Text>
+                            <Text style={styles.messageTextInbox}>
+                                {item.txt} {' '}
+                                {item.productRequestedID && (
+                                    <Text style={styles.productText}>
+                                        - {findProductById(item.productRequestedID)?.productName}
+                                    </Text>
+                                )}
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                )}
+            />
 
         </View>
     );
